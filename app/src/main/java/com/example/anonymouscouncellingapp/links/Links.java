7b671401cc10ef8007c5700f6fed95919729d2c0
novelCrescend0 <2694546@students.wits.ac.za>
2024-05-02 11:46:59 +0000

    trying to add a search bar that co-exits with the listview

--- app/src/main/java/com/example/anonymouscouncellingapp/links/Links.java
+++ app/src/main/java/com/example/anonymouscouncellingapp/links/Links.java
@@ -1,7 +1,7 @@
 package com.example.anonymouscouncellingapp.links;
 
 public class Links {
-    public static String LOGIN_PHP = "https://lamp.ms.wits.ac.za/home/s1858930/login.php";
-    public static String REGISTER_PHP = "https://lamp.ms.wits.ac.za/home/s1858930/register.php";
+    public static String login = "https://lamp.ms.wits.ac.za/home/s2694546/login.php";
+    public static String reg = "https://lamp.ms.wits.ac.za/home/s2694546/register.php";
     public static String probs = "https://lamp.ms.wits.ac.za/home/s2694546/problems.php";
 }
